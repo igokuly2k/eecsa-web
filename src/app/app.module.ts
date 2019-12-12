6ed639aa3166a7f698e0d0fb345c8e0acf5ca537
igokuly2k <igokulsmailbox@gmail.com>
2019-12-12 21:11:55 +0530

    Components Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { ServicesComponent } from './services/services.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactComponent,
+    JobsComponent,
+    ServicesComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
